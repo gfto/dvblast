2796334c33e0bbc2f7db2d76d938f0cf9454096a
Jean-Paul Saman <jpsaman@videolan.org>
2010-06-14 08:06:47 +0000

    version.h: bump version to 1.3.0-git

--- version.h
+++ version.h
@@ -22,6 +22,6 @@
  *****************************************************************************/
 
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 2
+#define VERSION_MINOR 3
 #define VERSION_REVISION 0
-#define VERSION_EXTRA ""
+#define VERSION_EXTRA "-git"
