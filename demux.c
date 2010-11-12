0bfd39d35f82b46714c1767e1d4fdcec7dd0aafd
Christophe Massiot <massiot@via.ecp.fr>
2010-11-12 22:09:50 +0000

    * demux.c: Add a missing include file.

--- demux.c
+++ demux.c
@@ -24,6 +24,7 @@
  *****************************************************************************/
 
 #include <stdlib.h>
+#include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <string.h>
