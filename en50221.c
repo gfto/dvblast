124d18f37ec7453154e65480e354a34b94658ce0
Julian Gardner <joolzg@btinternet.com>
2013-10-24 01:31:50 +0200

    Out of range check fix (spotted by Coverity: CID 1100931)

--- en50221.c
+++ en50221.c
@@ -575,7 +575,7 @@ static void SessionOpen( access_t * p_access, uint8_t i_slot,
         if ( !p_sessions[i_session_id - 1].i_resource_id )
             break;
     }
-    if ( i_session_id == MAX_SESSIONS )
+    if ( i_session_id > MAX_SESSIONS )
     {
         msg_Err( p_access, "too many sessions !" );
         return;
@@ -633,7 +633,7 @@ static void SessionCreate( access_t * p_access, int i_slot, int i_resource_id )
         if ( !p_sessions[i_session_id - 1].i_resource_id )
             break;
     }
-    if ( i_session_id == MAX_SESSIONS )
+    if ( i_session_id > MAX_SESSIONS )
     {
         msg_Err( p_access, "too many sessions !" );
         return;
