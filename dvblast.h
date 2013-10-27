65b717b1095adc17f2ffed6abbfce1e7d639fa8f
Georgi Chorbadzhiyski <gf@unixsol.org>
2013-10-27 21:34:33 +0200

    Fix compilation under OSX.
    OS X (and FreeBSD) have their own "strict ip" definition. It is
    binary compatible with Linux "struct iphdr".

--- dvblast.h
+++ dvblast.h
@@ -49,6 +49,22 @@ struct udpheader { // FAVOR_BSD hell ...
   u_int16_t check;
 };
 
+#if defined(__FreeBSD__) || defined(__APPLE__)
+struct iphdr {
+    unsigned int ihl:4;
+    unsigned int version:4;
+    uint8_t tos;
+    uint16_t tot_len;
+    uint16_t id;
+    uint16_t frag_off;
+    uint8_t ttl;
+    uint8_t protocol;
+    uint16_t check;
+    uint32_t saddr;
+    uint32_t daddr;
+};
+#endif
+
 struct udprawpkt {
     struct  iphdr iph;
     struct  udpheader udph;
