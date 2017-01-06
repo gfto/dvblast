gfto/dvblast

commit 161bc13c452b0400136287f6f2708a18d18b57f9

fix bug introduced in 8ad3fd69
The EITs were printed too often due to a mix up between EIT schedules and p/f.

demux.c
@@ -3001,6 +3001,9 @@ static void HandleEIT( uint16_t i_pid, uint8_t *p_eit, mtime_t i_dts )
         return;
     }
 
+    if ( i_table_id != EIT_TABLE_ID_PF_ACTUAL )
+        goto out_eit;
+
     /* We do not use psi_table_* primitives as the spec allows for holes in
      * section numbering, and there is no sure way to know whether you have
      * gathered all sections. */
@@ -3016,16 +3019,13 @@ static void HandleEIT( uint16_t i_pid, uint8_t *p_eit, mtime_t i_dts )
     free(p_sid->pp_eit_sections[i_section]);
     p_sid->pp_eit_sections[i_section] = p_eit;
 
-    if ( i_table_id == EIT_TABLE_ID_PF_ACTUAL )
+    eit_print( p_eit, msg_Dbg, NULL, demux_Iconv, NULL, PRINT_TEXT );
+    if ( b_print_enabled )
     {
-        eit_print( p_eit, msg_Dbg, NULL, demux_Iconv, NULL, PRINT_TEXT );
-        if ( b_print_enabled )
-        {
-            eit_print( p_eit, demux_Print, NULL,
-                       demux_Iconv, NULL, i_print_type );
-            if ( i_print_type == PRINT_XML )
-                fprintf(print_fh, "\n");
-        }
+        eit_print( p_eit, demux_Print, NULL,
+                   demux_Iconv, NULL, i_print_type );
+        if ( i_print_type == PRINT_XML )
+            fprintf(print_fh, "\n");
     }
 
 out_eit:

commit 6b35ca012e33e46ab6160b47debce50d62a302f9

fix memory leak introduced in 161bc13
When EIT schedules are selected.

demux.c
@@ -3030,6 +3030,8 @@ static void HandleEIT( uint16_t i_pid, uint8_t *p_eit, mtime_t i_dts )
 
 out_eit:
     SendEIT( p_sid, i_dts, p_eit );
+    if ( i_table_id != EIT_TABLE_ID_PF_ACTUAL )
+        free( p_eit );
 }
 
 /*****************************************************************************